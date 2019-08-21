470cffb94736c1ef71d833b8ea0f0ff62dcf5ff2
Hisham.Maged22 <hisham.maged10@gmail.com>
2019-08-21 21:23:38 +0200

    Capturing Groups and Alternation Section

---
+++ HackerRank/Regex/Grouping and Capturing/Capturing & Non-Capturing Groups/CaptureGroup.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class CaptureGroup
+{
+	public static void main(String[] args)
+	{
+		RegexTester tester = new RegexTester();
+		tester.check("(ok){3,}");
+	}
+
+
+}
+
+class RegexTester
+{
+	public void check(String regexPattern){
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("String Input: ");
+		String testString = input.nextLine();
+		Pattern p = Pattern.compile(regexPattern);
+		Matcher m = p.matcher(testString);
+		System.out.println("Matches result: "+(m.matches()?"There's a Match!!":"Nothing Matches"));
+		System.out.println("LookingAt result: "+(m.lookingAt()?"There's a Match!!":"Nothing Matches"));
+		/*while(m.find())
+		{
+			System.out.println(m.group() + "Found at index "+m.start()+" and ends at "+(m.end()-1));
+		}*/
+	}
+
+}
